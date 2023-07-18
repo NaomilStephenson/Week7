833f50e77ae62c62ed36652ae21df04a72b56a4a
= <=>
2023-07-18 10:27:55 +0100

    Test Server Hi & Bye!

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ let app = express();
 const server = app.listen((4000), ()=>{
     console.log("welcome to Nationwide server, we are listening...");
 });
+
+app.get("/hello",(request,response)=>{
+    response.send("Hay there - nice day!");
+});
+
+app.get("/bye",(request,response)=>{
+    response.send("See ya later, Aligator!");
+});
