c13a409c8004d1b90cdf74be04cac5a73be6f812
= <=>
2023-07-18 11:39:45 +0100

    params explained

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
+'use strict'
+
 let express = require("express");
 let app = express();
 const server = app.listen((4000), ()=>{
-    console.log("welcome to Nationwide server, we are listening...");
+    console.log("Welcome to Nationwide server, we are listening...");
 });
+
+let bye = () => {
+    let option = Math.floor(Math.random()*8);
+    let byeOptions = [
+        "See You Later, Alligator!",
+        "In A While, Crocodile!",
+        "Gotta Go, Buffalo!",
+        "See You Soon, Raccoon!",
+        "Be Sweet, Parakeet!",
+        "Take Care, Polar Bear!",
+        "In A Shake, Rattle Snake!",
+        "Hit The Road, Mr. Toad!",
+    ];
+    return byeOptions[option];
+};
+
+function sayBye(request,response){
+    response.send(bye());
+};
 
 app.get("/hello",(request,response)=>{
-    response.send("Hay there - nice day!");
+    response.send("Welcome to Nationwide!");
 });
 
-app.get("/bye",(request,response)=>{
-    response.send("See ya later, Aligator!");
-});
+app.get("/goodbye",sayBye);
+app.get("/bye",sayBye);
