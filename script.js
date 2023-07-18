9e3f9ba76429f38ce8fa568da582e9ea97af370c
Nay <136731795+NaomilStephenson@users.noreply.github.com>
2023-07-18 13:28:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -10,6 +10,21 @@ app.get("/hello",(request,response)=>{
     response.send("Welcome to Nationwide!");
 });
 
+let bye = () => {
+    let option = Math.floor(Math.random()*8);
+    let byeOptions = [
+        "See You Later, Alligator!",
+        "After A While, Crocodile!",
+        "Gotta Go, Buffalo!",
+       "See You Soon, Raccoon!",
+        "Be Sweet, Parakeet!",
+        "Take Care, Polar Bear!",
+        "In A Shake, Garter Snake!",
+        "Hit The Road, Happy Toad!"
+    ];
+    return byeOptions[option];
+};
+
 app.get("/bye",(request,response)=>{
-    response.send("See ya later, Aligator!");
+    response.send(bye());
 });
